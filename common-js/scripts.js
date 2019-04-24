a5d46897a7feb3499c1319c469dfed518fe7f22b
Guilherme <tec.guilherme.dsc@gmail.com>
2019-04-23 22:11:42 -0300

    redes sociais

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -46,6 +46,13 @@
 })(jQuery);
 
 
+var $doc = $('html, body');
+$('a').click(function() {
+    $doc.animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 800);
+    return false;
+})
 
 function countdownTime(){
 	
